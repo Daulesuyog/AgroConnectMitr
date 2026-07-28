4b625ae0a496e9d2428411e5f76b84069524a1fb
Daulesuyog <suyogdaule2807@gmail.com>
2026-07-28 17:58:44 +0530

    Fix database connection

--- Backend/Models/AgroConnectMitr.js
+++ Backend/Models/AgroConnectMitr.js
@@ -12,20 +12,19 @@
 
 import pg from "pg";
 
-const db =
-  process.env.NODE_ENV === "production"
-    ? new pg.Client({
-        connectionString: process.env.Database_url,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      })
-    : new pg.Client({
-        user: "postgres",
-        host: "localhost",
-        database: "AgroConnectMitr",
-        password: "12345678",
-        port: 5432,
-      });
+const db = process.env.Database_url
+  ? new pg.Client({
+      connectionString: process.env.Database_url,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new pg.Client({
+      user: "postgres",
+      host: "localhost",
+      database: "AgroConnectMitr",
+      password: "12345678",
+      port: 5432,
+    });
 
 export default db;
